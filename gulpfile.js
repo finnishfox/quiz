a062d8b5dec0d973ddbdc8ee9890500225af9783
finnishfox <onefinnishfox@gmail.com>
2017-06-20 14:36:28 +0300

    react added

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,34 @@ const sass = require('gulp-sass');
 const concatCss = require('gulp-concat-css');
 const concatJs = require('gulp-concat');
 const runSequence = require('run-sequence');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const glob = require('glob');
+
 
 gulp.task('sass-to-css', () => gulp.src('source/components/**/*.scss').pipe(sass()).pipe(concatCss('bundle.css'))
-  .pipe(gulp.dest('css')));
+  .pipe(gulp.dest('source/css')));
 
-gulp.task('concat-js', () => gulp.src('source/components/**/*.js')
-  .pipe(concatJs('script.js'))
-  .pipe(gulp.dest('js/')));
+// gulp.task('concat-js', () => {
+//   return gulp.src('source/components/**/*.js')
+//     .pipe(concatJs('script.js'))
+//     .pipe(gulp.dest('source/js'));
+// });
 
+gulp.task('build-js', function () {
+  const files = glob.sync('./source/components/**/*.js');
+
+  return browserify({entries: files, extensions: ['.js']})
+    .transform('babelify', {presets: ['es2015', 'react']})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./source/js'));
+});
 
 
 gulp.task('default', () => {
-  runSequence( 'sass-to-css', 'concat-js');
+  runSequence('sass-to-css','build-js');
 });
 
 gulp.watch('source/components/**/*.*', ['default']);
